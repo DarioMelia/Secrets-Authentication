53cce274e6d72030d326a092801c41709319dbd2
DarioMeliaBasus <dario@meliabasus.com>
2021-07-05 23:03:54 +0200

    Add sign in with google,facebook and discord(not working) usin OAuth20

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const DiscordStrategy = require("passport-discord").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 // const bcrypt = require("bcryptjs");                  //Salting and Hashing
 // const sha512 = require('js-sha512').sha512;         //Hashing, better than MD5
 // const encrypt = require("mongoose-encryption");    //Encryption
@@ -37,9 +41,15 @@ mongoose.connect(process.env.DB_URL, {
   useCreateIndex: true
 });
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  googleId:String,
+  facebookId: String,
+  discordID: String,
+  username:String
+});
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
@@ -48,8 +58,63 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+///////////// SOCIAL MEDIA LOGINS SET-UPS //////////////////////////////////
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile,email, cb) {
+
+    console.log(email.emails[0].value);
+    User.findOrCreate({ googleId: profile.id, username: email.emails[0].value}, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken,profile, cb) {
+    // console.log(email);
+    console.log(profile);
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+var discordScopes = ['identify', 'email'];
+var prompt = "consent";
+
+passport.use(new DiscordStrategy({
+    clientID: process.env.DISCORD_CLIENT_ID,
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/discord/secrets",
+    scope: discordScopes,
+    prompt: prompt
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log(profile)
+    User.findOrCreate({ discordId: profile.id }, function(err, user) {
+        return done(err, user);
+    });
+}));
 
 
 ////////////// REQUESTS ////////////////
@@ -73,6 +138,29 @@ app.get("/login", function(req, res) {
 });
 logErrorMessage = undefined;
 });
+
+app.get("/auth/google",passport.authenticate("google", {scope:["profile", "email"] }));
+app.get("/auth/google/secrets",
+passport.authenticate("google", {failureRedirect: "/login"}),
+(req,res) => {
+  res.redirect("/secrets");
+});
+
+app.get("/auth/facebook",passport.authenticate("facebook", {scope:["public_profile"]}));
+app.get("/auth/facebook/secrets",
+passport.authenticate("facebook", { failureRedirect: "/login" }),
+(req, res) => {
+  res.redirect("/secrets");
+});
+
+
+app.get('/auth/discord', passport.authenticate('discord',{scopes:discordScopes, prompt:prompt}));
+app.get('/auth/discord/secrets', passport.authenticate('discord', {
+    failureRedirect: '/'
+}), function(req, res) {
+    res.redirect('/secrets') // Successful auth
+});
+
 
 app.get("/secrets", function(req, res){
   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stal   e=0, post-check=0, pre-check=0');
